8fed09a1b0aaf3f29b201df05158a84a3884d96d
Ninjoe <joequah1@gmail.com>
2025-10-23 16:37:24 +0800

    fix

--- app/(main)/inventory/[id]/page.tsx
+++ app/(main)/inventory/[id]/page.tsx
@@ -1,10 +1,9 @@
+"use client";
+import { useParams } from 'next/navigation';
 import InventoryDetailPage from '../InventoryDetailPage';
 
-interface PageProps {
-  params: Promise<{ id: string }>;
-}
-
-export default async function Page({ params }: PageProps) {
-  const { id } = await params;
+export default function Page() {
+  const params = useParams();
+  const id = Array.isArray(params.id) ? params.id[0] : params.id as string;
   return <InventoryDetailPage id={id} />;
 }
